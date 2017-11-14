a86c598d4ee68946e278a951678145c44eebcede
Qiulonghui <569374939@qq.com>
2017-11-14 16:22:43 +0800

    20171114

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,18 +1,39 @@
 <template>
 	<div class="goods">
-		我是goods
+		<div class="menu-wrapper">
+			
+		</div>
+		<div class="foods-wrapper"></div>
 	</div>
 </template>
 
 <script type="text/ecmascript-6">
 	export default{
 		props:{
-			detail:{
+			seller:{
 				type:Object
 			}
+		},
+		created:function(){
+			
 		}
 	};
 </script>
 
-<style lang="stylus" rel="stylesheet/stylus">
+<style type="text/css">
+.goods{
+	display: flex;
+	position:absolute;
+	top: 174px;
+	bottom: 46px;
+	width: 100%;
+	overflow: hidden;
+}
+.goods .menu-wrapper{
+	width: 80px;
+	background: #f3f5f7;
+}
+.goods .foods-wrapper{
+	flex: 1;
+}
 </style>
--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -29,30 +29,40 @@
 		<div class="background">
 			<img v-bind:src="seller.avatar" width="100%" />
 		</div>
-		<div v-show="detailShow" class="detail">
-			<div class="detail-wrapper clearfix">
-				<div class="detail-main">
-					<h1 class="name">{{seller.name}}</h1>
-					<div class="star-wrapper">
-						<star v-bind:size="48" v-bind:score="seller.score"></star>
+		<transition name="fade">
+			<div v-show="detailShow" class="detail">
+				<div class="detail-wrapper clearfix">
+					<div class="detail-main">
+						<h1 class="name">{{seller.name}}</h1>
+						<div class="star-wrapper">
+							<star v-bind:size="48" v-bind:score="seller.score"></star>
+						</div>
+						<div class="title">
+							<div class="line"></div>
+							<div class="text">优惠信息</div>
+							<div class="line"></div>
+						</div>
+						<ul v-if="seller.supports" class="supports"> 
+							<li class="support-item" v-for="(item,index) in seller.supports">
+								<span class="icon" v-bind:class="classMap[seller.supports[index].type]"></span>
+								<span class="text">{{seller.supports[index].description}}</span>
+							</li>
+						</ul>
+						<div class="title">
+							<div class="line"></div>
+							<div class="text">商家公告</div>
+							<div class="line"></div>
+						</div>
+						<div class="bulletin">
+							<p class="content">{{seller.bulletin}}</p>
+						</div>
 					</div>
-					<div class="title">
-						<div class="line"></div>
-						<div class="text">优惠信息</div>
-						<div class="line"></div>
-					</div>
-					<ul v-if="seller.supports" class="support"> 
-						<li class="support-item" v-for="(item,index) in seller.supports">
-							<span class="icon" v-bind:class="classMap[seller.supports[index]]"></span>
-							<span class="text">{{seller.supports[index].description}}</span>
-						</li>
-					</ul>
+				</div>
+				<div class="detail-close" v-on:click="hideDetail">
+					<i class="icon-close"></i>
 				</div>
 			</div>
-			<div class="detail-close">
-				<i class="icon-close"></i>
-			</div>
-		</div>
+		</transition>
 	</div>
 </template>
 
@@ -66,12 +76,15 @@
 		},
 		data:function(){
 			return {
-				detailShow:true
+				detailShow:false
 			}			
 		},
 		methods:{
 			showDetail:function(){
 				this.detailShow = true;
+			},
+			hideDetail:function(){
+				this.detailShow = false;
 			}
 		},
 		created:function(){
@@ -231,25 +244,10 @@
 		        top: -6px
 		        border-bottom: 1px solid rgba(255,255,255,0.2)
 		      .text
-	            padding:0 12px
-	            font-weight: 700
-	            font-size: 14px
-            .supports
-              width: 80%
-              margin: 0 auto
-              .support-item
-                padding: 0 12px
-                margin-bottom: 12px
-                font-size: 0
-                &:last-child
-                  margin-bottom: 0
-                .icon
-                  display: inline-block
-                  width: 16px
-                  height: 16px
-                  vertical-align: top
-                  margin-right: 6px
-                  background-size: 16px 16px            
+	          padding: 0 12px  	
+	          font-weight: 700
+	          font-size: 14px
+		      
 	  .detail-close
 	    position: relative
 	    width: 32px
@@ -261,4 +259,68 @@
 
 <style type="text/css">
 	@import url("../../common/stylus/style.css");
+	.supports{
+		width: 80%;
+		margin: 0 auto;
+	}
+	.supports .support-item{
+		padding: 0 12px;
+		margin-bottom: 12px;
+		font-size: 0;	    
+	}
+	.supports .support-item:last-child{
+		margin-bottom: 0;
+	}
+	.supports .support-item .icon{
+		display: inline-block;
+  	width: 16px;
+  	height: 16px;
+  	vertical-align: top;
+  	margin-right: 6px;
+  	background-size: 16px 16px;
+  	background-repeat: no-repeat;
+	}
+	.supports .support-item .icon.decrease{
+		background-image: url(decrease_2@2x.png);
+	}
+	.supports .support-item .icon.discount{
+		background-image: url(discount_2@2x.png);
+	}
+	.supports .support-item .icon.guarantee{
+		background-image: url(guarantee_2@2x.png);
+	}
+	.supports .support-item .icon.invoice{
+		background-image: url(invoice_2@2x.png);
+	}
+	.supports .support-item .icon.special{
+		background-image: url(special_2@2x.png);
+	}
+	.supports .support-item .text{
+		line-height: 16px;
+    font-size: 12px;
+	}
+	.supports .support-item .detail-close{
+		position: relative;
+    width: 32px;
+    height: 32px;
+    margin: -64px auto 0 auto;
+    clear: both;
+    font-size: 32px;
+	}
+	.bulletin{
+		width: 80%;
+		margin: 0 auto;
+	}
+	.bulletin .content{
+		padding: 0 12px;
+		line-height: 24px;
+		font-size: 12px;
+		margin: 0;
+	}
+	.fade-enter-active, .fade-leave-active {
+	  transition: opacity .5s;
+	}
+	.fade-enter, .fade-leave-to{
+	  opacity: 0;
+	}
 </style>
